c6a1f162bf9931819d55b43757cfa86e56b4fc86
Luis Enrique De Jesus <nouveau.lookup4g@icloud.com>
2026-09-24 13:58:32 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine(CountDigits(2));
+
+
+static int CountDigits(int numbers)
+{
+    if (numbers < 10)
+    {
+        // Console.WriteLine("1");
+        return 1;
+    }
+    else
+    {
+        var xl = 1 + CountDigits(numbers / 10);
+        //Console.WriteLine(xl);
+        return xl;
+    }
+}
+
